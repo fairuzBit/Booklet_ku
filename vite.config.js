1c3e8aeb46daa2cb79d377cdae3f0b06ce6a46c1
fairuzBit <afzulfigd@gmail.com>
2025-11-30 22:29:47 +0700

    cleaning package.lock json

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-<<<<<<< HEAD
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-=======
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
->>>>>>> teman/main
-})
+  plugins: [react(), tailwindcss()],
+});
